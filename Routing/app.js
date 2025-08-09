9ceeb815ff184d48b6311ae05cbf359327a84da3
Jose Valduz <jose.valduz@unet.edu.ve>
2025-08-09 14:40:25 -0400

    Routing en Node.js
    Mi primer servidor y API
    manejo de GET y POST

--- Routing/app.js
+++ Routing/app.js
@@ -7,11 +7,56 @@ const servidor= http.createServer((req,res)=>{
     switch (method) {
         case 'GET':
             return manejarSolicitudGET(req,res);
+        case 'POST':
+            return manejarSolicitudPOST(req,res);
         default:
-            console.log(`El metodo no puede ser manejado por el servidor: ${method}`)
-            break;
+            res.statusCode=501;
+            res.end(`El metodo no puede ser manejado por el servidor: ${method}`)
+            
     }
 });
+
+function manejarSolicitudGET(req,res){
+    const path= req.url;
+    // console.log(res.statusCode);
+    if (path ==='/') {
+        // res.writeHead(200, {'Content-Type':'aplication/json'});
+        res.statusCode=200;
+        return res.end('Bienvenidos a mi primer servidor y API creados con Node.js');
+    }else if(path === '/cursos'){
+        res.statusCode=200;
+        return res.end(JSON.stringify(cursos.infoCursos));
+    }else if (path==='/cursos/programacion'){
+        req.statusCode=200;
+        return res.end(JSON.stringify(cursos.infoCursos.programacion));
+    }
+    res.statusCode=404;
+    res.end('El recurso solicitado no existe...')
+}
+
+function manejarSolicitudPOST(req,res){
+    const path =req.url;
+    
+    if (path === '/cursos/programacion') {
+        // res.statusCode=200;
+        let cuerpo = '';
+        req.on('data',contenido =>{
+            cuerpo+=contenido.toString();
+        });
+        req.on('end',() =>{
+            console.log(cuerpo);
+            console.log(typeof cuerpo);
+            //Convertir a un objeto de javascript
+            cuerpo = JSON.parse(cuerpo);
+            console.log(typeof cuerpo);
+            console.log(cuerpo.titulo);
+            res.end('El servidor recibio una solicitud POST para /cursos/programacion');
+        });
+
+        // res.end('El servidor recibio una solicitud POST para /cursos/programacion')
+    }
+}
+
 const PUERTO =3000;
 servidor.listen(PUERTO,()=>{
     console.log(`El servidor esta escuchando en el puerto ${PUERTO}`);
